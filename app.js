67cbf34db04e9605e2a628f86645931d5c976fd5
Kitti.Bank <kittitach.pro@gmail.com>
2025-02-19 14:36:14 +0700

    add index.html

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const chalk = require('chalk')
 const debug = require('debug')('app');
-const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname,"/public/")));
+
 app.get("/", (req,res) => {
     res.send('Hello Express');
-
 
 })
 
